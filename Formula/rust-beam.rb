8c8244133de3b15219c76078dd7f603411aced5f
Jurgen <hanekom.jurgen@bcg.com>
2024-06-17 22:07:20 +0800

    fix release

--- Formula/rust-beam.rb
+++ Formula/rust-beam.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Espanso < Formula
+class RustBeam < Formula
     desc "My simple file transfer thing written in Rust"
     homepage "https://github.com/jhanekom27/rust-beam"
     url "https://github.com/jhanekom27/rust-beam/releases/download/latest/rust-beam-mac.tar.gz"
