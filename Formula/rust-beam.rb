65ae447e66f8d881e8347bfca4a0ebcb8a5b56e0
Jurgen <hanekom.jurgen@bcg.com>
2024-06-17 22:18:05 +0800

    fix releae url

--- Formula/rust-beam.rb
+++ Formula/rust-beam.rb
@@ -4,7 +4,7 @@
 class RustBeam < Formula
     desc "My simple file transfer thing written in Rust"
     homepage "https://github.com/jhanekom27/rust-beam"
-    url "https://github.com/jhanekom27/rust-beam/releases/download/latest/rust-beam-mac.tar.gz"
+    url "https://github.com/jhanekom27/rust-beam/releases/latest/download/rust-beam-mac.tar.gz"
     sha256 "1bafb49b9930293a3b0d055690f560e56ae3960713e1660678711d12a23a637e"
     version "0.0.1"
   
