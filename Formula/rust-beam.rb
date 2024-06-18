3263b66e219e4a481ceefc1ec1860c17e609e963
Jurgen <hanekom.jurgen@bcg.com>
2024-06-18 13:26:58 +0800

    v0.0.2 sha

--- Formula/rust-beam.rb
+++ Formula/rust-beam.rb
@@ -5,8 +5,8 @@ class RustBeam < Formula
     desc "My simple file transfer thing written in Rust"
     homepage "https://github.com/jhanekom27/rust-beam"
     url "https://github.com/jhanekom27/rust-beam/releases/latest/download/rust-beam-mac.tar.gz"
-    sha256 "1bafb49b9930293a3b0d055690f560e56ae3960713e1660678711d12a23a637e"
-    version "0.0.1"
+    sha256 "2ddcae12d409ad05289bc623a545c8ffb8e3fa650502ec015c11ac11e62ffce2"
+    version "0.0.2"
   
     def install
       bin.install "rust-beam"
