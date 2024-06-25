9a6dc53f00dac4dbbf6145c74abd7b0575d06d16
Jurgen <hanekom.jurgen@bcg.com>
2024-06-25 11:32:15 +0800

    fix: formula update script

--- Formula/rust-beam.rb
+++ Formula/rust-beam.rb
@@ -4,14 +4,14 @@
 class RustBeam < Formula
   desc "My simple file transfer thing written in Rust"
   homepage "https://github.com/jhanekom27/rust-beam"
-  version "formula-update"
+  version "0.0.30"
 
   if OS.mac?
     url "macurl"
-    sha256 ""
+    sha256 "MACOS_SHA256_CHECKSUM"
   elsif OS.linux?
-    url "https://github.com/user/repo/releases/download/vformula-update/rust-beam-formula-update-linux.tar.gz"
-    sha256 ""
+    url "https://github.com/user/repo/releases/download/v1.0.0/rust-beam-1.0.0-linux.tar.gz"
+    sha256 "LINUX_SHA256_CHECKSUM"
   end
 
   def install
